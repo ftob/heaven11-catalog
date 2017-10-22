15cd6aae2edeca6b189cb852341cc90e0e1ee843
Nikita Volkov <litebackend@gmail.com>
2017-10-22 23:20:05 +0700

    Bridge ampq2graylog

---
+++ templates/bridge/0/README.md
@@ -0,0 +1,1 @@
+## Bridge RabbitMQ to GrayLog
---
+++ templates/client/0/php/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+# Create /ram directory if the enrivonment variable RAM_SIZE is set
+if [ ! -z "$RAM_SIZE" ]
+then
+    RAM_SIZE=${RAM_SIZE:-"256"}
+
+    if [ ! -d "/ram" ]; then
+        mkdir /ram
+    fi
+    mount -t tmpfs -o size="${RAM_SIZE}m" tmpfs /ram
+fi
+
+# Start php-fpm
+cd /opt/app && composer install && php-fpm --allow-to-run-as-root
